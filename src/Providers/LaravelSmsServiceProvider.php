81febcf2bb78fd67d51564395fb0fc8f8043120e
Nazmul Alam <nazmulpcc@gmail.com>
2021-03-30 16:52:38 +0600

    Initializing from laratie

---
+++ src/Providers/LaravelSmsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Nazmulpcc\LaravelSms\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
